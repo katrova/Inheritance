affbc071d5b05daa03000e842d371eea2c349a43
Kate <Kate@MacBook-Pro-Catherine.local>
2020-06-08 22:48:35 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/*
+ * Classname : Inheritance
+ *
+ * 8 June 2020
+ *
+ * Copyright Vasylkivska Kateryna KNTEU
+ *
+ * Module 2 task 2
+ *
+ * Inheritance
+
+   1. Create a child  from the class Rectangle.
+
+   2. Create another class as a super class for your one.  Or create a daughterly class for your one.
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
